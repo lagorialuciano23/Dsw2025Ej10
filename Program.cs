269020c61891c67ad7d78ee1fb814ef10454450b
Francisco Vicente <fvicente@totalcoin.com>
2025-05-08 20:13:49 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Dsw2025Ej10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* A partir de la lista de productos que se obtiene de Producto.CrearListaDeEjemplo()
+             * Resolver:
+             * 1. El primer producto 
+             * 2. El último producto
+             * 3. La suma de precios
+             * 4. El promedio de precios
+             * 5. Listar los productos con Id mayor a 15
+             * 6. Obtener una lista de cada producto con su nombre y el precio en formato moneda, luego mostrar los elementos
+             * 7. El producto con el precio más alto
+             * 8. El producto con el precio más bajo
+             * 9. Obtener y mostrar los productos cuyo precio sea mayor al promedio
+             * 10. Listar los productos ordenados por descripción de forma descendente
+             * Cada punto se debe realizar en un método distinto, en una nueva clase llamada Solucion
+             */
+        }
+    }
+}
